6c3ff7103867e2d307c0d895f986a4e8121d94b1
Isaac Karam <isaackaram@outlook.com>
2020-03-09 03:35:33 +0200

    Initial Commit

---
+++ Roomy/RoundedButton.swift
@@ -0,0 +1,58 @@
+//
+//  CornerButton.swift
+//  Roomy
+//
+//  Created by User on 3/9/20.
+//  Copyright © 2020 User. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class RoundedButton: UIButton {
+
+    @IBInspectable var cornerRadius: CGFloat = 0{
+        didSet{
+        self.layer.cornerRadius = cornerRadius
+        }
+    }
+
+    @IBInspectable var borderWidth: CGFloat = 0{
+        didSet{
+            self.layer.borderWidth = borderWidth
+        }
+    }
+
+    @IBInspectable var borderColor: UIColor = UIColor.clear{
+        didSet{
+            self.layer.borderColor = borderColor.cgColor
+        }
+    }
+    
+    @IBInspectable var shadowColor: UIColor = UIColor.clear{
+        didSet{
+            self.layer.shadowColor = shadowColor.cgColor
+        }
+    }
+    
+    
+    @IBInspectable var shadowRadius: CGFloat = 0{
+        didSet{
+            self.layer.shadowRadius = shadowRadius
+        }
+    }
+    
+    @IBInspectable var shadowOpacity: Float = 0{
+        didSet{
+            self.layer.shadowOpacity = shadowOpacity
+        }
+    }
+    @IBInspectable var shadowOffset: CGSize = CGSize(width: 0, height: 0){
+        didSet{
+            self.layer.shadowOffset = shadowOffset
+        }
+    }
+    
+
+
+}
---
+++ Roomy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Roomy
+//
+//  Created by User on 3/9/20.
+//  Copyright © 2020 User. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
